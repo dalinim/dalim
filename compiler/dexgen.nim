66630dbe3ea617310bd3c908475561cde894ba18
Richard Jansson <richard.j.jansson@gmail.com>
2019-04-01 19:42:09 +0300

    iterate over statement lists

--- compiler/dexgen.nim
+++ compiler/dexgen.nim
@@ -16,14 +16,26 @@ proc newModule(g: ModuleGraph; module: PSym): BModule =
   new(result)
   result.graph = g
   result.module = module
+
+proc procNode(n: PNode) =
+  case n.kind:
+  of nkStmtList,nkStmtListExpr:
+    echo "Statement list: "
+    for i in countup(0,n.sonsLen-1):
+      procNode(n.sons[i])
+  else:
+    echo n.kind
   
 # Open, Process and Close are used to create pass object
 proc dexOpen(graph: ModuleGraph; module: PSym): PPassContext =
   result = newModule(graph,module)
 
 proc dexProcess(b: PPassContext, n: PNode): PNode =
-  echo n.kind
-  echo "@" & $n.info.line & ":" & $n.info.col
+#  echo n.kind
+#  echo "@" & $n.info.line & ":" & $n.info.col
+
+  procNode(n)
+
   result = n
 
 proc dexClose(graph: ModuleGraph; b: PPassContext, n: PNode): PNode =
