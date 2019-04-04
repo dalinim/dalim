e80b8568a5f08c4a6d2ecb3616e622e0a00b0655
Mateusz Czapliński <czapkofan@gmail.com>
2019-04-04 22:13:47 +0200

    dexgen: emit simple const-wide/32 opcode

--- compiler/dexgen.nim
+++ compiler/dexgen.nim
@@ -1,5 +1,5 @@
 import
-  ast, passes, lineinfos, tables, hashes
+  ast, idents, passes, lineinfos, tables, hashes
 
 # debug 
 import typetraits
@@ -22,49 +22,59 @@ type
     nextReg: int               # index of the first register not matched to a variable yet
     locals: Table[string, PVar]  # local variables
 
-proc getReg(p: PProc, v: PSym): string =
-  let id = v.name.s
+proc getReg(p: PProc, v: PIdent): string =
+  let id = v.s
   if not p.locals.contains(id):
     p.locals[id] = PVar(reg0: p.nextReg)
     inc(p.nextReg)
   let l = p.locals[id]
   return "v" & $l.reg0
 
-proc genVarInit(p: PProc, v: PSym, n: PNode) =
-  # TODO: super simplified for initial PoC
-  if n.kind == nkIntLit:
-    echo ".. const-wide/32 " & getReg(p, v) & ", " & $n.intVal
+# proc genVarInit(p: PProc, v: PSym, n: PNode) =
+#   # TODO: super simplified for initial PoC
+#   if n.kind == nkIntLit:
+#     echo ".. const-wide/32 " & getReg(p, v) & ", " & $n.intVal
 
-proc genVarStmt(p: PProc, n: PNode) =
-  for i in countup(0, sonsLen(n) - 1):
-    var a = n.sons[i]
-    if a.kind != nkCommentStmt:
-      if a.kind == nkVarTuple:
-        discard
-      #   let unpacked = lowerTupleUnpacking(p.module.graph, a, p.prc)
-      #   genStmt(p, unpacked)
-      else:
-        assert(a.kind == nkIdentDefs)
-        assert(a.sons[0].kind == nkSym)
-        var v = a.sons[0].sym
-        # if lfNoDecl notin v.loc.flags and sfImportc notin v.flags:
-        #   genLineDir(p, a)
-        genVarInit(p, v, a.sons[2])
+# proc genVarStmt(p: PProc, n: PNode) =
+#   for i in countup(0, sonsLen(n) - 1):
+#     var a = n.sons[i]
+#     if a.kind != nkCommentStmt:
+#       if a.kind == nkVarTuple:
+#         discard
+#       #   let unpacked = lowerTupleUnpacking(p.module.graph, a, p.prc)
+#       #   genStmt(p, unpacked)
+#       else:
+#         assert(a.kind == nkIdentDefs)
+#         assert(a.sons[0].kind == nkSym)
+#         var v = a.sons[0].sym
+#         # if lfNoDecl notin v.loc.flags and sfImportc notin v.flags:
+#         #   genLineDir(p, a)
+#         genVarInit(p, v, a.sons[2])
 
-proc gen(p: PProc, n: PNode #[, r: var TCompRes]# ) =
-  ## Main code generation "switch" procedure; based on gen() in jsgen.nim
+proc match(p: PProc, n: PNode, indent: string) =
+  ## Main code generation "switch" procedure; inspired by gen() in jsgen.nim
+  echo indent, n.kind
   case n.kind
-  of nkStmtList, nkStmtListExpr:
-    let isExpr = not isEmptyType(n.typ)
-    for i in countup(0, sonsLen(n) - 1 - isExpr.ord):
-      # genStmt(p, n.sons[i])
-      gen(p, n.sons[i])
-    # if isExpr:
-    #   gen(p, lastSon(n), r)
-    # echo repr(n)
-  of nkVarSection, nkLetSection: genVarStmt(p, n)
+  of nkStmtList, nkStmtListExpr, nkVarSection, nkEmpty, nkInfix:
+    for i in countup(0, sonsLen(n) - 1):
+      match(p, n.sons[i], indent & " ")
+    # let isExpr = not isEmptyType(n.typ)
+    # for i in countup(0, sonsLen(n) - 1 - isExpr.ord):
+    #   # genStmt(p, n.sons[i])
+    #   gen(p, n.sons[i])
+    # # if isExpr:
+    # #   gen(p, lastSon(n), r)
+    # # echo repr(n)
+  # of nkVarSection, nkLetSection: genVarStmt(p, n)
+  of nkIdent:
+    echo indent, " s=", repr(n.ident.s)
+  of nkIntLit:
+    echo indent, " intVal=", n.intVal
+  of nkIdentDefs:
+    if n.sons[0].kind == nkIdent and n.sons[2].kind == nkIntLit:
+      echo ".. const-wide/32 " & getReg(p, n.sons[0].ident) & ", " & $n.sons[2].intVal
   else:
-    echo n.kind
+    discard
 
 proc newModule(g: ModuleGraph; module: PSym): BModule =
   new(result)
@@ -95,17 +105,18 @@ proc dexProcess(b: PPassContext, n: PNode): PNode =
   # jsgen.nim:
   # - entry point call path to gen(): myProcess() -> genModule() -> genStmt() -> gen()
 
-  var p = PProc(
-    locals: initTable[string, PVar](),
-  )
-  gen(p, n)
-
 #  echo n.kind
 #  echo "@" & $n.info.line & ":" & $n.info.col
 
   procNode(n)
 
   result = n
+
+  var p = PProc(
+    locals: initTable[string, PVar](),
+  )
+  match(p, n, " ")
+
 
 proc dexClose(graph: ModuleGraph; b: PPassContext, n: PNode): PNode =
   echo "STUB: close"
--- quick.sh
+++ quick.sh
@@ -1,3 +1,3 @@
 set -xe
 compiler/nim0 c --nimcache:nimcache/d_linux_amd64 compiler/nim.nim
-compiler/nim1 dex --skipcfg ${1:-arithm.nim}
+compiler/nim dex --skipcfg ${1:-arithm.nim}
